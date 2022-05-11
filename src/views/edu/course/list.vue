5ada87756a9d8b04c568f9d98300834578bf4607
zj-fight <2976008778@qq.com>
2022-05-11 14:28:01 +0800

    v1.2

--- src/views/edu/course/list.vue
+++ src/views/edu/course/list.vue
@@ -43,12 +43,12 @@
       <el-table-column label="操作" width="200" align="center">
         <template slot-scope="scope">
           <router-link :to="'/teacher/edit/' + scope.row.id">
-            <el-button type="primary" size="mini" icon="el-icon-edit"
+            <el-button type="text" size="mini" icon="el-icon-edit"
               >编辑课程基本信息</el-button
             >
           </router-link>
           <router-link :to="'/teacher/edit/' + scope.row.id">
-            <el-button type="primary" size="mini" icon="el-icon-edit"
+            <el-button type="text" size="mini" icon="el-icon-edit"
               >编辑课程大纲息</el-button
             >
           </router-link>
