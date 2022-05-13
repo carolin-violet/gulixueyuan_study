76505651d2a779d3af5e068db784b91a7f44c259
zj-fight <2976008778@qq.com>
2022-05-13 20:26:58 +0800

    v-1.1

---
+++ api/banner.js
@@ -0,0 +1,12 @@
+import request from "@/utils/request"
+
+export default {
+
+  getListBanner() {
+    return request({
+      url: '/educms/bannerfront/getAllBanner',
+      method: 'get'
+    })
+  },
+
+}
